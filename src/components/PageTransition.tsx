cbfd79256af553db9505d831a5d93a7eb9a1162f
Tahaa <bukharitahaali@gmail.com>
2026-01-06 02:58:25 +0500

    Update PageTransition.tsx

--- src/components/PageTransition.tsx
+++ src/components/PageTransition.tsx
@@ -11,9 +11,9 @@ export default function PageTransition({ children }: { children: ReactNode }) {
     <AnimatePresence mode="wait">
       <motion.div
         key={pathname}
-        initial={{ opacity: 0, x: 40 }}
+        initial={{ opacity: 1, x: 0 }}
         animate={{ opacity: 1, x: 0 }}
-        exit={{ opacity: 0, x: -40 }}
+        exit={{ opacity: 0, x: -20 }}
         transition={{ duration: 0.3, ease: 'easeInOut' }}
         className="min-h-screen flex flex-col"
       >
